afe0c391c15145a359dc6a22f3b3b6f3f6b5f234
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-07-03 02:48:39 +0100

    Update route table cidrs

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "this_private" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block     = each.value
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.this.id
   }
 
@@ -84,7 +84,7 @@ resource "aws_route_table" "this_public" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block = each.value
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
 
