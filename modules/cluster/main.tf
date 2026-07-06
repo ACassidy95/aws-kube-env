a56a4de201d1c98319150b45d1d08ff750e2e64f
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-07-06 18:27:43 +0100

    Add default node group specification to cluster module with necessary IAM role and policy attachments

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -3,17 +3,61 @@ resource "aws_eks_cluster" "this" {
   version = var.cluster_version
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    endpoint_private_access = false
+    endpoint_public_acces   = true
+    subnet_ids              = var.subnet_ids
   }
 
   role_arn = aws_iam_role.this.arn
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
 
   // Create role attachment before cluster and destroy it after, otherwise
   // EKS controller may lose ability to interact with EKS-managed EC2 infra
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "aws_iam_role" "this" {
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  version         = var.cluser_version
+  node_group_name = "${var.base_name}-node-group"
+  node_role_arn   = aws_iam_role.node.arn
+
+  subnet_ids     = var.subnet_ids
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.large"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 6
+    min_size     = 1
+  }
+
+  updae_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.container_registry_RO_policy,
+  ]
+
+  # Allow external changes without TF plan difference, i.e. let cluster-autoscaler
+  # or karpenter do their work.
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+}
+
+resource "aws_iam_role" "cluster" {
   name = "${var.base_name}-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -31,7 +75,41 @@ resource "aws_iam_role" "this" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+resource "aws_iam_role" "node" {
+  name = "${var.base_name}-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.this.arn
+  role       = aws_iam_role.cluster.arn
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.arn
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.arn
+}
+
+resource "aws_iam_role_policy_attachment" "container_registry_RO_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.arn
 }
