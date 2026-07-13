92bdc2629434dba34b0855af1f7701d584b21864
Aaron Cassidy <aaron.cassidy1995@gmail.com>
2026-07-13 23:38:05 +0100

    Fix admin role references

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -16,19 +16,19 @@ resource "aws_iam_policy" "admin" {
   policy = data.aws_iam_policy_document.admin.json
 }
 
+resource "aws_iam_policy" "assume_admin" {
+  name   = "AmazonEKSAssumeAdminPolicy"
+  policy = data.aws_iam_policy_document.assume_admin.json
+}
+
 resource "aws_iam_policy" "viewer" {
   name   = "AmazonEKSViewerPolicy"
   policy = data.aws_iam_policy_document.viewer.json
 }
 
-resource "aws_iam_policy" "manager" {
-  name   = "AmazonEKSManagerPolicy"
-  policy = data.aws_iam_policy_document.manager.json
-}
-
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = aws_iam_role.admin.name
-  policy_arn = aws_iam_policy.viewer.arn
+  policy_arn = aws_iam_policy.admin.arn
 }
 
 resource "aws_iam_user_policy_attachment" "viewer" {
@@ -38,7 +38,7 @@ resource "aws_iam_user_policy_attachment" "viewer" {
 
 resource "aws_iam_user_policy_attachment" "manager" {
   user       = aws_iam_user.manager.name
-  policy_arn = aws_iam_policy.manager.arn
+  policy_arn = aws_iam_policy.assume_admin.arn
 }
 
 resource "aws_eks_access_entry" "viewer" {
@@ -49,6 +49,6 @@ resource "aws_eks_access_entry" "viewer" {
 
 resource "aws_eks_access_entry" "manager" {
   cluster_name      = var.cluster_name
-  principal_arn     = aws_iam_user.manager.arn
+  principal_arn     = aws_iam_role.admin.arn
   kubernetes_groups = var.manager_kubernetes_groups
 }
